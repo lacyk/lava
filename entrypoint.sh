46360f45c51113ebe7818da791eb4c41494c5fb4
amos <99amos@gmail.com>
2022-08-30 16:22:13 +0300

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-#!bin/sh
+#!bin/bash
 
+cd ../app/target
 java -Ddatasource.dialect=HSQLDB\
-                -Ddatasource.url=jdbc:hsqldb:mem:lavagna\ 
-                -Ddatasource.username=user\
-                -Ddatasource.password=user\
-                -Dspring.profile.active=dev\
-                -jar lavagna-jetty-console.war
+ -Ddatasource.url=jdbc:hsqldb:mem:lavagna\
+ -Ddatasource.username=user\
+ -Ddatasource.password=user\
+ -Dspring.profile.active=dev\
+ -jar lavagna-jetty-console.war
